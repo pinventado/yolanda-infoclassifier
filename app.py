7d2d41ca351086589af4e28d232551399e2f1d20
Paul Inventado <paul.inventado@delasalle.ph>
2013-11-16 15:21:37 +0900

    Fixed saving of binary data

--- app.py
+++ app.py
@@ -26,10 +26,11 @@ except IOError:
 #  main():
 #
 from gevent import monkey; monkey.patch_all()
+from pymongo import binary
 import bottle, zlib, pymongo, json, base64
 
 def mongo_store(tweet, args):
-   tbl.insert({'data':base64.b64encode(zlib.compress(json.dumps(tweet)))})
+   tbl.insert({'data':binary.Binary(base64.b64encode(zlib.compress(json.dumps(tweet))))})
 
 
 if __name__ == '__main__':
